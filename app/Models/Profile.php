b27df4a060fc788eff82c44dd1374d3ffe598f55
Benjamin Angella <90790158+UBenTech@users.noreply.github.com>
2026-03-04 02:59:52 +0300

    Fix Profile primary key for PostgreSQL seeding/login

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -10,6 +10,12 @@ class Profile extends Model
 {
     use HasFactory;
 
+    protected $primaryKey = 'user_id';
+
+    public $incrementing = false;
+
+    protected $keyType = 'int';
+
     protected $fillable = [
         'user_id',
         'avatar_url',
